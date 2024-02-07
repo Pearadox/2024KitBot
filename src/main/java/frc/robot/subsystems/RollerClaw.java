0cfaf9c676e62c29da22a6ab789e04dad753edb5
Nazeer <46301909+nh17000@users.noreply.github.com>
2024-02-06 19:50:14 -0600

    Update RollerClaw.java
    Make it not not work

--- src/main/java/frc/robot/subsystems/RollerClaw.java
+++ src/main/java/frc/robot/subsystems/RollerClaw.java
@@ -2,20 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+package frc.robot.subsystems;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.lib.drivers.PearadoxSparkMax;
-// import frc.robot.Constants.RollerClawConstants;
+import frc.robot.Constants.RollerClawConstants;
 
 public class RollerClaw extends SubsystemBase {
   //maybe change to a VictorSPX later (brushed)
   VictorSPX rollerClaw;
   /** Creates a new RollerClaw. */
   public RollerClaw() {
-    rollerClaw = new VictorSPX(LauncherConstants.launcherID);
+    rollerClaw = new VictorSPX(RollerClawConstants.rollerClawID);
 
     rollerClaw.configFactoryDefault();
 
@@ -31,3 +31,9 @@ public class RollerClaw extends SubsystemBase {
   public void stop() {
     rollerClaw.set(ControlMode.PercentOutput, 0);
   }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
